c2e17deb59fd94223f6e286d641cca7075bcc9eb
Pendragon0988 <Creative@Creatives-iMac.local>
2015-11-12 16:31:23 +0800

    Fixed whole app

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,18 +1,46 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
-  console.log("Hello from the controller!");
+  console.log("Hello Creative from the controller");
 
-  $http.get('/contactlist').success(function(response) {
-    console.log("I got the data I requested");
+var refresh = function() {
+  $http.get('/contactlist').success(function(response){
+    console.log("I got the data requested");
     $scope.contactlist = response;
-  });
+    $scope.contact = "";
+  })
+}
+
+refresh();
 
   $scope.addContact = function() {
     console.log($scope.contact);
     $http.post('/contactlist', $scope.contact).success(function(response){
       console.log(response);
+      refresh();
     });
+  };
+
+  $scope.remove = function(id) {
+    console.log(id);
+    $http.delete('/contactlist/' + id).success(function(response){
+      refresh();
+    });
+  };
+
+  $scope.edit = function(id) {
+    console.log(id);
+    $http.get('/contactlist/' + id).success(function(response){
+      $scope.contact = response;
+    });
+  };
+
+  $scope.update =  function() {
+    console.log($scope.contact._id);
+    $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(resposne){
+      refresh();
+    })
   }
+
 
 }]);
--- server.js
+++ server.js
@@ -1,30 +1,72 @@
-var express = require('express');
-var app     = express();
-var mongojs = require('mongojs');
-var db      = mongojs('contactlist', ['contactlist']);
+var express    = require('express'),
+    app        = express(),
+    mongojs    = require('mongojs'),
+    db         = mongojs('contactlist', ['contactlist']),
+    bodyParser = require('body-parser');
 
-
-/*
- * Step 1: Test the server connection
- *
- * app.get('/', function(req, res){
-   res.send("Hello Creative from the server!");
-   });
-
-
- */
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get('/contactlist', function(req, res){
   console.log("I received a GET request");
+  /*person1 = {
+    name: 'Tom',
+    email: 'tom@email.com',
+    number: '(111)111-1111'
+  },
+    person2 = {
+    name: 'Tracy',
+    email: 'tracy@email.com',
+    number: '(222)222-222'
+  },
+    person3 = {
+    name: 'Tucker',
+    email: 'tucker@email.com',
+    number: '(333)333-333'
+  }
+
+  var contactlist = [person1, person2, person3];
+  res.json(contactlist);*/
 
   db.contactlist.find(function(err, docs){
     console.log(docs);
     res.json(docs);
   });
+});
 
+app.post('/contactlist',function(req,res){
+  db.contactlist.insert({name: req.body.name, email: req.body.email, number: req.body.number},function(err, doc){
+    res.json(doc);
+  });
+});
+
+app.delete('/contactlist/:id', function(req, res){
+  var id = req.params.id;
+  console.log(id);
+  db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+    res.json(doc);
+  });
+});
+
+app.get('/contactlist/:id', function(req, res){
+  var id = req.params.id;
+  console.log(id);
+  db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
+    res.json(doc);
+  });
+});
+
+app.put('/contactlist/:id', function(req, res){
+  var id = req.params.id;
+  console.log(req.body.name);
+  db.contactlist.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+    new: true }, function(err, doc) {
+      res.json(doc);
+  });
 });
 
 app.listen(9000);
-console.log("Server running on port over 9000...");
+console.log("Server is listening to port over 9000...");
